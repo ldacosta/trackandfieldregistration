cb4d63b9c00b4f8de5f7e38f3fbd74dc7286c03a
Luis Da Costa <dacosta.le@gmail.com>
2014-05-09 11:14:40 -0400

    Put JavaScript code in its own file

---
+++ script.js
@@ -0,0 +1,7 @@
+<!--
+
+//-->
+function toggleTable(eventName) {
+    var lTable = document.getElementById(eventName);
+    lTable.style.display = (lTable.style.display == "table") ? "none" : "table";
+}
