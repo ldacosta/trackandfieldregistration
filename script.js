b832b8559d3ae8e2305b4178dbcdc32486443a51
Luis Da Costa <dacosta.le@gmail.com>
2014-05-14 15:28:28 -0400

    Change events' row styles on 'entering' click

--- script.js
+++ script.js
@@ -10,6 +10,39 @@ var EventSummaryHandler =
 {
  		init: function() 
 		{
+		 // control of summary rows for different events		 
+		 // 100 m
+		 document.getElementById("enter100mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('100m', true); })
+		 document.getElementById("enter100mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('100m', false); })
+		 // 200 m
+		 document.getElementById("enter200mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('200m', true); })
+		 document.getElementById("enter200mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('200m', false); })
+		 // 400 m
+		 document.getElementById("enter400mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('400m', true); })
+		 document.getElementById("enter400mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('400m', false); })
+		 // 800 m
+		 document.getElementById("enter800mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('800m', true); })
+		 document.getElementById("enter800mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('800m', false); })
+		 // 1500 m
+		 document.getElementById("enter1500mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('1500m', true); })
+		 document.getElementById("enter1500mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('1500m', false); })
+		 // 3000 m
+		 document.getElementById("enter3000mYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('3000m', true); })
+		 document.getElementById("enter3000mNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('3000m', false); })
+		 // High Jump
+		 document.getElementById("enterHighJumpYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('HighJump', true); })
+		 document.getElementById("enterHighJumpNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('HighJump', false); })
+		 // Long Jump
+		 document.getElementById("enterLongJumpYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('LongJump', true); })
+		 document.getElementById("enterLongJumpNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('LongJump', false); })
+		 // Triple Jump
+		 document.getElementById("enterTripleJumpYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('TripleJump', true); })
+		 document.getElementById("enterTripleJumpNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('TripleJump', false); })
+		 // Shot Put 
+		 document.getElementById("enterShotPutYes").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('ShotPut', true); })
+		 document.getElementById("enterShotPutNo").onclick = (function() { EventSummaryHandler.setProperClassOnEventSummary('ShotPut', false); })
+		 // CHANGE STYLE OF 'summaryShotPut'
+		 // control of checkboxes showing 'tables' to enter athletes
 		 document.getElementById("show100m").onclick = (function() { EventSummaryHandler.toggleEventTable('100m'); })
 		 document.getElementById("show200m").onclick = (function() { EventSummaryHandler.toggleEventTable('200m'); })
 		 document.getElementById("show400m").onclick = (function() { EventSummaryHandler.toggleEventTable('400m'); })
@@ -23,6 +56,17 @@ var EventSummaryHandler =
 		}, 
 
 
+		// puts appropriate style on event's summary row
+		setProperClassOnEventSummary: function(eventName, entering)
+		{
+		 var rowName = "summary" + eventName
+		 if (entering) 
+		 		document.getElementById(rowName).setAttribute("class", "incompleteEvent")
+		 else
+		 		document.getElementById(rowName).setAttribute("class", "toIgnoreEvent")
+		},
+		
+		// shows/hide table of a specific event, to enter athletes
 		toggleEventTable: function(eventName) 
 		{
 		 var lTable = document.getElementById(eventName);
